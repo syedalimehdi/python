ecdee2e4078fbe0d24ae6f44b7dffd2a4b70a395
syedalimehdi <mehdisyed@gmail.com>
2020-07-28 15:32:10 -0700

    Update 2-List.py

--- 2-List.py
+++ 2-List.py
@@ -1,11 +1,9 @@
-        "#List and List Comprehension example\n",
-        "#just using List for movies starting with G\n",
-        "\n",
-        "movies = [\"DDLJ\", \"Gandhi\", \"Kabhi Kushi Kabhie Gham\"]\n",
-        "\n",
-        "gmovies=[]\n",
-        "for title in movies:\n",
-        "  if title.startswith(\"G\"):\n",
-        "    gmovies.append(title)\n",
-        "  print(gmovies)"
+      "#List and List Comprehension example\n",
+       "#just using List for movies starting with G\n",
+       movies = ["DDLJ", "Gandhi", "Kabhi Kushi Kabhie Gham"]
+       gmovies=[]
+        for title in movies:
+                if title.startswith("G")
+                gmovies.append(title)
+        print(gmovies)
  
